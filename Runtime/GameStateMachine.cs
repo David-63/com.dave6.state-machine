7f6f35d3069133fa3f4202a4cf234216aeb143f4
David 6 <jiseop.63@gmail.com>
2025-12-07 19:01:09 +0900

    change display name

--- Runtime/GameStateMachine.cs
+++ Runtime/GameStateMachine.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Dave6.StateMachine
 {
-    public class StateMachine
+    public class GameStateMachine
     {
         StateNode currentNode;
         Dictionary<Type, StateNode> nodes = new();
